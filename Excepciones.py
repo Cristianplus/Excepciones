70fcf595298ccebe5efe358a7a2c1b05ef873cf3
Cristian Cordero <cristiancordero2810@gmail.com>
2025-07-22 04:05:09 -0500

    Ejercicio de practica: captura de excepciones, manejo de errores, acción de no error y comandos que se ejecutan con o sin errores

--- Excepciones.py
+++ Excepciones.py
@@ -6,4 +6,38 @@ try:
     #print(10/0)
     print([1,2,3,4][4])
 except Exception as error:
-    print(f"Se ha producido un error: {error}")
+    print(f"Se ha producido un error: {error} ({type(error).__name__})")
+
+
+print("----- EXTRA -----")
+
+class StrTypeError(Exception):
+    pass
+
+def process_params(parameters: list):
+    if len(parameters) < 3:
+        raise IndexError()
+    elif parameters[1] == 0:
+        raise ZeroDivisionError()
+    elif type(parameters[2]) == str:
+        raise StrTypeError(
+            "El tercer elemento no puede ser una cadena de texto")
+    
+    print(parameters[2])
+    print(parameters[0]/parameters[1])
+    print(parameters[2] + 5)
+
+try:
+    process_params([1, 2, 3, 4])
+except IndexError as e:
+    print("El número de elementos de la lista debe ser mayor que dos")
+except ZeroDivisionError as e:
+    print("El segundo elemento de la lista no puede ser cero")
+except StrTypeError as e:
+    print(f"Error: {e}")
+except Exception as e:
+    print(f"Se ha producido un error inesperado: {e}")
+else:
+    print("No se ha producido ningún error.")
+finally:
+    print("El programa finaliza sin detenerse.")
